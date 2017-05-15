1889559359d9fc389e299060c917ba916a886923
Wellming Li <wellming.li@howzhi.com>
2017-05-16 01:21:08 +0800

    add ci

---
+++ database.sql
@@ -0,0 +1,8 @@
+CREATE TABLE `cache_sync` (
+  `id` int(10) unsigned NOT NULL AUTO_INCREMENT,
+  `op` enum('set','del') NOT NULL COMMENT 'Cache操作',
+  `k` varchar(1024) NOT NULL DEFAULT '' COMMENT 'Cache的Key',
+  `v` text COMMENT '当op为set时，v为set的值',
+  `created_time` int(10) unsigned NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=663 DEFAULT CHARSET=utf8;
